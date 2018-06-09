e886163bf6cb044d52056eab879b9d67168c714e
Zbyszek Wieczorek <dafi@dafik.tk>
2018-06-09 11:04:48 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fromEvent } from "rxjs/observable/fromEvent";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+
+  }
 }
